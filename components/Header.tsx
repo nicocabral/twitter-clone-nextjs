13e4096f08ed7098e32832ccddd69eda52517a11
Nico Cabral <ncabral010694@gmail.com>
2023-04-22 22:39:44 +0800

    Add modal and hooks

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router";
 import React, { useCallback } from "react";
+import { BiArrowBack } from "react-icons/bi";
 
 interface HeaderProps {
     label: string;
@@ -18,8 +19,22 @@ const Header: React.FC<HeaderProps> = ({
         <div className="
             border-b[1px] border-neutral-800 p-5
         ">
-            <div>
-                
+            <div className="flex flex-items items-center gap-2">
+                {
+                    showBackArrow && (
+                        <BiArrowBack
+                            onClick={handleBack}
+                            color="white"
+                            size={20}
+                            className="
+                            cursor-pointer
+                            hover:opacity-70
+                            transition
+                            "
+                        />
+                    )
+                }
+                <h1 className="text-white text-xl font-semibold">{label}</h1>
             </div>
             {label}
         </div>
